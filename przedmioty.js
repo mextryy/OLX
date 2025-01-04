dae0d01923de13e3933acb84d66125b97bf15f09
mextryy <atryndoch@student.agh.edu.pl>
2025-01-04 16:30:45 +0100

    chaotyczne jak zawsze zmiany w projekcie,wyszukiwanie po nazwie przedmiotów , dodawanie przedmiotów

---
+++ przedmioty.js
@@ -0,0 +1,28 @@
+
+// Tablica przedmiotów
+const products = [
+    {
+        nazwa: "Buty do biegania",
+        cena: 150.99,
+        kolor: "czarne",
+        stan: "nowe",
+        marka: "Nike",
+        kategoria: "Sport",
+        opis: "Super wygodne",
+        dostepnosc: true,
+        zdjecie: ""
+    },
+    {
+        nazwa: "Buty do biegania",
+        cena: 150.99,
+        kolor: "czarne",
+        stan: "nowe",
+        marka: "Nike",
+        kategoria: "Sport",
+        opis: "Super wygodne",
+        dostepnosc: true,
+        zdjecie: ""
+    }
+];
+// Eksportowanie tablicy przedmiotów
+export { products };
